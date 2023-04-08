edcffb487785e7b5a07914c33cf3d862d3ebff3c
swaroopsonline <swaroopsonline@gmail.com>
2023-04-09 00:05:42 +1000

    8th April commit

---
+++ FindingNosDivisibleInRangeInReverseOrder.js
@@ -0,0 +1,13 @@
+/*--------This Code will find the Nos divisible by 3 between 500 to 700 in reverse order----------*/
+/*-------------------------------------Dated: 8th April 2023--------------------------------------*/
+
+
+
+for(let i = 700;i > 500; i--){
+
+    if(i%3==0){
+        console.log(i)
+
+    }
+
+}
